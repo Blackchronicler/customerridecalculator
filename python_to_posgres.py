cac61ee471476b79daa3059d4aaed82a01d2400a
Black Chronicler <69725403+Blackchronicler@users.noreply.github.com>
2022-07-03 21:14:53 +0200

    TA| updated connect to DB

--- python_to_posgres.py
+++ python_to_posgres.py
@@ -5,16 +5,28 @@ database = "ty_psql"
 username = "postgres"
 pwd = "admin"
 port_id = 5432
+conn = None
+cur = None
+
+try:
+    conn = psycopg2.connect(
+        host = hostname,
+        dbname = database,
+        user = username,
+        password = pwd,
+        port = port_id
+    )
+
+    cur = conn.cursor()
 
 
-conn = psycopg2.connect(
-    host = hostname,
-    dbname = database,
-    user = username,
-    password = pwd,
-    port = port_id
-)
+    cur.close()
+    conn.close()
 
-
-
-conn.close()
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
